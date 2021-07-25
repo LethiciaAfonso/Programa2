25c28213c5bee89b786f7210409156e25e916d56
LethiciaAfonso <86988473+LethiciaAfonso@users.noreply.github.com>
2021-07-25 18:15:34 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+package programa;
+
+import java.util.List;
+
+import classes.Motorista;
+import classes.Passageiro;
+import classes.Pessoa;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pessoa passageiro = new Passageiro("", "","");  
+        ((Passageiro)passageiro).setCodigo(1);           
+    }for(
+
+    int i = 0;i<produto.length;i++)
+    {
+        System.out.println("Digite o Cpf" );
+         cpf = cpf.next();
+ 
+}
